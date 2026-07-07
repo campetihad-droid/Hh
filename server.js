1fd90407f78bea66dd305dc7dd613afb882fa4eb
campetihad-droid <campetihad@gmail.com>
2026-07-07 23:02:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server Running");
 });
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHANNEL_ID = "-1003924350648";
+
+const bot = new TelegramBot(BOT_TOKEN, {
+  polling: true
+});
+
+let running = false;
+let timer = null;
+let userLastUsed = {};
+
+function generateRandomUserId() {
